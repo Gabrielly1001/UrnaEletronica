477b847fe1d2eba6bf8fbda187d23ecc527b2725
Gabrielly Roza <gabrielly.roza@icloud.com>
2021-09-13 20:36:01 -0300

    first commit

---
+++ etapas.js
@@ -0,0 +1,48 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 38111,
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos:[
+                    {URL: 'oi.jpg', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero: 77222,
+                nome: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos:[
+                    {URL: 'um.jpg' , legenda: 'Vereador'}
+                ]
+              },
+    {
+    titulo: 'PREFEITO',
+    numero: 2,
+    candidatos: [
+        {
+            numero: 99,
+            nome: 'Ciclano',
+            partido: 'ABC',
+            vice: 'Cic',
+            fotos:[
+                {URL: 'um.jpg', legenda: 'Prefeito'},
+                {URL: 'oi.jpg', legenda: 'Vice-Prefeito', small: true}
+            ]
+        },
+        {
+           numero: 84,
+           nome: 'Zulano',
+           partido: 'QUERTY',
+           vice: 'Zull',
+           fotos:[
+               {url: 'oi.jpg', legenda: 'Prefeito'},
+               {url: 'um.jpg', legenda: 'Vice-Prefeito', small: true}
+           ] 
+        },
+       ]
+    }
+};
